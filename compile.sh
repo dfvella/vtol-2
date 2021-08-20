8074034ed9354bffd50d5b674f86d414138ed124
David Vella <davella@umich.edu>
2021-08-20 17:17:50 -0400

    improve compile.sh

--- compile.sh
+++ compile.sh
@@ -33,5 +33,18 @@ PROGRAM_END=$(objdump --all $TARGET_ELF | grep flash_binary_end | cut -d' ' -f1)
 XIP_BASE="10000000"
 PROGRAM_SIZE=$((16#$PROGRAM_END - 16#$XIP_BASE))
 
+# check if the program size interferes with flash logging
+TARGET_SRC=$(find . -type f -name $BUILD_TAR.c)
+USING_LOGGING=$(grep '#include "logging.h"' $TARGET_SRC)
+if [ -n "$USING_LOGGING" ]; then
+    LOG_FLASH_START=$(grep '#define LOG_FLASH_START' src/logging.h | cut -d' ' -f3)
+    LOG_FLASH_START=$(( $LOG_FLASH_START * 1024 ))
+    if [ $PROGRAM_SIZE -gt $LOG_FLASH_START ]; then
+        echo "error: program interferes with flash logging"
+        echo "program uses the first $PROGRAM_SIZE Bytes of flash but flash logging start at $LOG_FLASH_START"
+        exit 1
+    fi
+fi
+
 echo "compilation complete"
 echo "program $BUILD_TAR uses $PROGRAM_SIZE Bytes of flash"
